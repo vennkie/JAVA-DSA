c4d59d65e7ca6685adb7c7787a243e3351075db7
Venkatesh <161022907+vennkie@users.noreply.github.com>
2024-12-21 21:12:12 +0530

    1

---
+++ src/org/study/Car.java
@@ -0,0 +1,8 @@
+package org.study;
+
+public class Car {
+    private String doors;
+    private String color;
+    private String engine;
+    public int speed;
+}
---
+++ src/org/study/array.java
@@ -0,0 +1,9 @@
+package org.study;
+
+public class array {
+    public static void main(String[] args){
+        Car car=new Car();
+        car.speed=100;
+        System.out.println(car.speed);
+    }
+}
